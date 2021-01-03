395dbbe81b7438d9fe3f87959bc7fe78fecdac17
Shivamghildiyal29 <76207805+Shivamghildiyal29@users.noreply.github.com>
2021-01-04 01:54:40 +0530

    Add files via upload

---
+++ firstInteger.py
@@ -0,0 +1,34 @@
+num = 3
+print(type(num))
+num1 = 3.2
+print(type(num1))
+#Special arethimetic operation
+#Floor division
+print(3/2) #gives a decimal value
+print(3//2) #gives a rounded off value
+
+#Exponent
+print(3*3) #product
+print(3**3) # exponent
+
+#modular
+print(11%2) #gives a remainder
+
+#incrementation
+num = num+ 1
+print(num)
+num +=1
+print(num)
+
+#absolute value method
+print(abs(-3))
+
+#rounding
+print(round(3.485))
+print(round(3.485, 2)) #rounding upto two decimal values
+
+#typecasting
+num1='120'
+num2='220'
+num3 = int(num1) + int(num2) #typecasting
+print(num3)

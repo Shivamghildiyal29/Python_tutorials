3f962d723e5abfee2f5cfdb16560e15cd31bd135
Shivamghildiyal29 <76207805+Shivamghildiyal29@users.noreply.github.com>
2021-01-23 03:02:29 +0530

    Add files via upload

---
+++ 1.creating plots.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+import seaborn as sns
+#defining data of x axis
+dev_x = [25, 26, 27, 28,29,30,31,32,33,34]
+#defining data of y axis
+dev_y= [31351,56161,61651,64165,16516,48161,31681,61316,68161,68561]
+#plotting the plot
+sns.lineplot(dev_x, dev_y)
+#displaying the plot
+plt.show()
+#df["Expen"]
+
